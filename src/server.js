2afd52ee11bb470116a789a1ae7a1f3d8424c0d6
Sclipper <mkopoi3@gmail.com>
2018-11-07 13:10:50 +0100

    .

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const app = express();
 app.use( express.static( `${__dirname}/../build` ) );
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, '../build/index.html'));
-})
+});
 app.listen(port);
